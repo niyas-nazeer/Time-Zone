317c1b0463875a76223b1f62a462e1262a436794
niyas-nazeer <niyasnazeer719@gmail.com>
2024-07-08 00:24:21 +0530

    image file added & nav bar set

--- time_zone/time_zone/settings.py
+++ time_zone/time_zone/settings.py
@@ -117,13 +117,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# settings.py
 
+# URL prefix for static files.
+STATIC_URL = '/static/'
+
+# Additional directories where static files are located.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'home/static'),
 ]
+
+# Absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Adjust path as needed
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
